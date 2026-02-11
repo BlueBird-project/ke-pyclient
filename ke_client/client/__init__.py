94cfd18c782bb2413bb086ecb60405cd13c341b2
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2026-02-11 14:35:44 +0100

    add class to public api

--- ke_client/client/__init__.py
+++ ke_client/client/__init__.py
@@ -2,7 +2,7 @@ from ._ki_utils import ki_object
 from ._split_uri import SplitURIBase, ki_split_uri
 from ._rdf_utils import rdf_nil, is_nil
 from ._ki_exceptions import KITypeError, KIError
-from ._ki_bindings import BindingsBase
+from ._ki_bindings import BindingsBase, TargetedBindings
 from ._ke_properties import KESettings, KnowledgeInteractionConfig
 from ._client import KEClient, OptionalLiteral, OptionalURIRef
 from ._ki_holder import KIHolder
