fcdf420949f98760a71d7dbce17659a1259bd941
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2026-07-14 18:10:00 +0200

    filter duplicates

--- ke_client/gp_ext/_semantic_utils.py
+++ ke_client/gp_ext/_semantic_utils.py
@@ -1,3 +1,4 @@
+import hashlib
 import logging
 import time
 from typing import List, Tuple, Dict, Optional, Union, Callable, Type, Iterable
@@ -28,6 +29,7 @@ class KIPattern:
     _ext_new_mapping: Optional[Dict[Node, Node]] = None
     _ext_all_mapping: Optional[Dict[Node, Node]] = None
     ext_gp_id: Optional[str] = None
+    _triple_hash: Optional[str] = None
 
     def __repr__(self):
         return f"{self.kb_id}:{self.ki_name}"
@@ -96,7 +98,16 @@ class KIPattern:
             _t = tuple([self._ext_all_mapping[s] if s in self._ext_all_mapping else s for s in t])
             # noinspection PyTypeChecker
             triples_all.append(_t)
-        return triples_all
+        sorted_triples = sorted(triples_all, key=lambda x: (str(x[0]), str(x[1])))
+
+        return sorted_triples
+
+    @property
+    def triples_ext_all_hash(self) -> str:
+        if self._triple_hash is None:
+            triple_str = " ".join(["::".join(t) for t in self.triples_ext_all])
+            self._triple_hash = hashlib.md5(triple_str.encode()).hexdigest()
+        return self._triple_hash
 
     # @property
     # def graph_pattern_ext(self) -> str:
@@ -250,19 +261,26 @@ class SemanticGPExt:
     def match_ki(self, ki_name: str, graph_pattern: GraphPattern, handler: Optional[Callable]) \
             -> List[KnowledgeInteraction]:
         additional_patterns: List[KnowledgeInteraction] = []
+        pattern_hashes = {}
         for i, sc in enumerate(self.sc_list):
             if self.kb_id != sc.knowledge_base_id:
                 ki_pattern: KIPattern = self.match_kb_ki(ki_name=ki_name, other_kb_id=sc.knowledge_base_id)
                 if ki_pattern is not None:
                     # todo: check if similar graph pattern hasn't been already added to the list
-                    new_pattern = [" ".join([t.n3() for t in triple]) + " . " for triple in ki_pattern.triples_ext_all]
-                    new_gp = GraphPattern(
-                        **{**graph_pattern.__dict__, "pattern": new_pattern})
-                    # graph_pattern=ki_pattern.graph_pattern_ext_all)
-                    ki = KnowledgeInteraction(ki_name=f"-EXT-{i}-{ki_name}", handler=handler,
-                                              ki_type=KnowledgeInteractionType.parse(ki_pattern.interaction_type),
-                                              graph_pattern=new_gp)
-                    additional_patterns.append(ki)
+                    if ki_pattern.triples_ext_all_hash in pattern_hashes:
+                        logging.debug(
+                            f"Duplicate ext pattern for {ki_pattern.ki_name} ({pattern_hashes[ki_pattern.triples_ext_all_hash]})")
+                    else:
+                        new_pattern = [" ".join([t.n3() for t in triple]) + " . " for triple in
+                                       ki_pattern.triples_ext_all]
+                        new_gp = GraphPattern(
+                            **{**graph_pattern.__dict__, "pattern": new_pattern})
+                        # graph_pattern=ki_pattern.graph_pattern_ext_all)
+                        ki = KnowledgeInteraction(ki_name=f"-EXT-{i}-{ki_name}", handler=handler,
+                                                  ki_type=KnowledgeInteractionType.parse(ki_pattern.interaction_type),
+                                                  graph_pattern=new_gp)
+                        additional_patterns.append(ki)
+                        pattern_hashes[ki_pattern.triples_ext_all_hash] = ki.ki_name
         return additional_patterns
 
     def match_kb_ki(self, ki_name: str, other_kb_id: str) \
@@ -291,7 +309,7 @@ class SemanticGPExt:
             # return {}
         # here starts pattern inference /extensions
         other_kb_patterns = [ki for ki in other_kb_cache.ki_patterns.values() if
-             KnowledgeInteractionType.is_opposite(ki.interaction_type, ki_pattern.interaction_type)]
+                             KnowledgeInteractionType.is_opposite(ki.interaction_type, ki_pattern.interaction_type)]
         if ke_settings.has_extend_graph_patterns_mode(GraphPatternExtMode.TRIPLE_MATCH):
             for other_pattern in other_kb_patterns:
                 # triple match - no sparql
