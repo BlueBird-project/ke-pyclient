3520e6a5da7f7ef4dc13d1fb267778c9880d1d99
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2026-08-20 16:19:01 +0200

    fix type

--- ke_client/ki_model.py
+++ ke_client/ki_model.py
@@ -153,7 +153,7 @@ class KnowledgeInteraction(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
     ki_name: str
     handler: Union[
-        Callable[[str, Optional[List[Dict[str, Any]]], str], Union[Dict[str, Any, str], List[Dict[str, Any, str]]]],
+        Callable[[str, Optional[List[Dict[str, Any]]], str], Union[Dict[str, Any], List[Dict[str, Any]]]],
         Callable[[], Union[Dict[str, Any], List[Dict[str, Any]]]],
         None
     ] = None
